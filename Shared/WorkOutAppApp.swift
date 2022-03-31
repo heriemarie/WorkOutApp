ce5eb3d5c17228924249d626f493ef956191d126
Herie <heriemarie@gmail.com>
2022-03-31 21:15:15 +0800

    Initial Commit

---
+++ Shared/WorkOutAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkOutAppApp.swift
+//  Shared
+//
+//  Created by Herie Marie A. Estaño on 3/31/22.
+//
+
+import SwiftUI
+
+@main
+struct WorkOutAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
